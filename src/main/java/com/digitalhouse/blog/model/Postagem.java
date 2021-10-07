278772f114e633aece79ef39ce986f5123807f17
erictomson <erictomson@gmail.com>
2021-10-07 14:44:45 -0300

    Configurados os parâmetros de conexão ao database e criada a classe Postagem

---
+++ src/main/java/com/digitalhouse/blog/model/Postagem.java
@@ -0,0 +1,43 @@
+package com.digitalhouse.blog.model;
+
+import java.sql.Date;
+
+public class Postagem {
+
+    private long id;
+    private String titulo;
+    private String texto;
+    private Date date = new Date(System.currentTimeMillis());
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
